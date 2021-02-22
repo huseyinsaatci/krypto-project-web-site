af2bd3d564b663f8a5a144fb1fb9750a9ba66e83
SHsaatcihuseyin <59334986+SHsaatcihuseyin@users.noreply.github.com>
2021-02-22 22:36:30 +0300

    c

--- server/server.js
+++ server/server.js
@@ -3,9 +3,9 @@ const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
-const x,
+const x;
 
-    app.use(express.static(publicPath));
+app.use(express.static(publicPath));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
